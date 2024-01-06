a1a2d212a52053f5767f0d186dd2386b16faf215
Andrew <zeyu.li23@gmail.com>
2024-01-06 16:11:50 -0600

    feat: updated with og images

--- src/components/constants/constants.tsx
+++ src/components/constants/constants.tsx
@@ -1,6 +1,6 @@
 const DEFAULT_TITLE = "Positron";
 const DEFAULT_DESCRIPTION = "Reduce the amount of hate and misinformation";
-const DEFAULT_BASE_URL = "https://positron.com/";
+const DEFAULT_BASE_URL = "https://positron-rouge.vercel.app/";
 
 const CONSTANTS = {
   DEFAULT_TITLE: DEFAULT_TITLE,
@@ -9,7 +9,7 @@ const CONSTANTS = {
   DEFAULT_KEYWORDS: ["Positron"],
   DEFAULT_SITE_NAME: "Positron",
   DEFAULT_BASE_URL: DEFAULT_BASE_URL,
-  DEFAULT_PREVIEW_IMG: "https://positron.com/ogimage.png",
+  DEFAULT_PREVIEW_IMG: "https://positron-rouge.vercel.app/ogimage.png",
   DEFAULT_TWITTER_TITLE: DEFAULT_TITLE,
   DEFAULT_TWITTER_DESCRIPTION: DEFAULT_DESCRIPTION,
   DEFAULT_GOOGLE_ANALYTICS: "G-KLVV6HWCRZ",
